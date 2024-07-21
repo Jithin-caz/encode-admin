35dc19d73389ddeeed30981beee6bfb433cc803e
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-07-21 13:26:06 +0530

    dlt

--- src/app/api/getTeams/route.ts
+++ src/app/api/getTeams/route.ts
@@ -21,27 +21,38 @@ console.log('in get getTeams')
 
 const  registered  =getRegistervalue(req.url) ;
   const teams=await db.collection('teams').find({ registered:registered }).toArray();
-  //  for(var i=0;i<teams.length;i++)
-  //  {
-  //    for(var j=0;j<teams[i].members.length;j++)
-  //    {
-  //      const user={
-  //        name:teams[i].members[j].name,
-  //        email:teams[i].members[j].email,
-  //        country:teams[i].members[j].country,
-  //        contact:teams[i].members[j].contact,
-  //        team:teams[i].members[j].team,
-  //      }
-  //      if(registered)
-  //      {
-  //        const res=await db.collection('registeredTeammembers').insertOne(user)
-  //      }
-  //      else
-  //      {
-  //        const res=await db.collection('notregisteredTeammembers').insertOne(user)
-  //      }
-  //    }
-  //  }
+    for(var i=0;i<teams.length;i++)
+    {
+      if(!teams[i].members)
+      {
+       // console.log('no member')
+        const filter={team:teams[i].team}
+ // console.log(team)
+    const result=await db.collection('teams').deleteOne(filter);
+       
+      }
+     else{
+      //console.log('got member')
+       // for(var j=0;j<teams[i].members.length;j++)
+        // {
+        //   const user={
+        //     name:teams[i].members[j].name,
+        //     email:teams[i].members[j].email,
+        //     country:teams[i].members[j].country,
+        //     contact:teams[i].members[j].contact,
+        //     team:teams[i].members[j].team,
+        //   }
+        //   if(registered)
+        //   {
+        //     const res=await db.collection('registeredTeammembers').insertOne(user)
+        //   }
+        //   else
+        //   {
+        //     const res=await db.collection('notregisteredTeammembers').insertOne(user)
+        //   }
+        // }
+     }
+    }
   //@ts-ignore
   return Response.json({
     message:"get success",
